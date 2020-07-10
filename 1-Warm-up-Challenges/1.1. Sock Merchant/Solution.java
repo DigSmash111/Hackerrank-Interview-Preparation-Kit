16407ddcff452aff3e5a10e0919565c6090f14bb
Diganta Som <itsdsom111@gmail.com>
2020-07-10 17:32:20 +0530

    added Warmup - Socks Merchant

---
+++ 1-Warm-up-Challenges/1.1. Sock Merchant/Solution.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+import javax.naming.spi.DirStateFactory.Result;
+import java.util.HashSet;
+
+public class Solution {
+  static int sockMerchant(int n, int[] ar) {
+    HashSet<Integer> set = new HashSet<Integer>();
+    int pair_count=0;
+    for (int i=0; i<n; i++) {
+      int element = ar[i];
+      if (set.contains(element)) {
+        set.remove(element);
+        pair_count++;
+      } else {
+        set.add(element);
+      }
+    }
+    return pair_count;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] ar = new int[n];
+    for (int i=0; i<n; i++) {
+      ar[i] = sc.nextInt();
+    }
+    int result = sockMerchant(n, ar);
+    System.out.println(result);
+    sc.close();
+  }
+}
